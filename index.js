b85cc702216ac4dc40de5614f3a0cf71918e320f
Brian Wong <brianw385@gmail.com>
2020-03-18 17:31:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var cart = [];
+//create items to be used globally
+var items =
 
 function getCart() {
  return cart;
@@ -10,10 +12,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+  var itemName = item;
+  var itemPrice = Math.random() * (100-1) + 1;
+  cart.push({'itemName': itemName, 'itemPrice': itemPrice});
+  return `${itemName} has been added to your cart.`;
  // write your code here
 }
 
 function viewCart() {
+  var array1 = []
+  for (var i = 1; i >= cart.length; i++) {
+    array.push(Object.keys(cart)[i]);
+    if (array1.length === 0) {
+      return 'Your shopping cart is empty.'}
+  } else (array.length === 1) {
+    return `In your cart, you have `array1`.`
+  }
   // write your code here
 }
 
